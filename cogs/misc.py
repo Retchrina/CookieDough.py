32e8405f1c18e76c0527f31fdd5707d3f44251b1
Airalyn <retchrina@gmail.com>
2025-10-16 00:20:02 -0700

    Updated Diaper Training Roles

--- cogs/misc.py
+++ cogs/misc.py
@@ -171,7 +171,7 @@ We had to move the list to [this google sheet](<https://bit.ly/3Bd1Zbq>), it doe
                 value="- If a trainee is asleep, they will be required to post their photo right when they wake up.\n- Trainees may provide preemptive photos if they'll be awake and unable to check Discord for ≥3 hours. When they return, they must provide another picture.\n -# A good reason and a detailed explanation must be provided to be granted amnesty. Mundane reasons won't be accepted.",
                 inline=False)
         embed.add_field(name="<:BridApplause:1356492855849385984> Users:",
-                value="<:MeruPolice:935948597063716874> **Enforcers**:\n<@178965982528798720>\n<:PaddedPat:635092900769693716> **Diaper Trainees**:\n<@210817997269499904> <@221774651012022273> <@235482330335019008>",
+                value="<:MeruPolice:935948597063716874> **Enforcers**:\n<@178965982528798720>\n<:PaddedPat:635092900769693716> **Diaper Trainees**:\n<@210817997269499904> <@270687710316855306> <@330299397457969152> <@263440152003608578>",
                 inline=False)
         embed.set_footer(text="This post was brought to you by Anti Big Toilet")
         try: # need to try since the command errors on hybrid invoke if ctx.invoked and ctx.role_id are undefied.
